2fae7b97a3bd36a94e0bd3a6b4675559161a8c9e
Caspianlake <imranhuseynguliyev@gmail.com>
2026-01-12 19:50:21 +0300

    Update terrain.gd

--- Scripts/Terrain/terrain.gd
+++ Scripts/Terrain/terrain.gd
@@ -10,22 +10,14 @@ extends Node3D
 ## Distance in chunks to render around the origin.
 @export var render_distance:= 16
 
-## Number of remaining tasks.
-var tasks_remaining: int = 0
-## Start time for performance measurement.
-var t_start: int
-
 ## Called when SDF generation ends.
 func sdf_gen_ended(data, chunk_key):
-	tasks_remaining -= 1
 	
 	if data:
 		ThreadPool.add_task(SurfaceNets.generate_mesh.bind(data,chunk_size+Vector3i(2,2,2),chunk_key))
-		tasks_remaining += 1
 
 ## Called when meshing ends.
 func meshing_ended(data, chunk_key):
-	tasks_remaining -= 1
 
 	if data is ArrayMesh:
 		var mi = MeshInstance3D.new()
@@ -34,19 +26,12 @@ func meshing_ended(data, chunk_key):
 		mi.position = Vector3(chunk_key.x * chunk_size.x, 0, chunk_key.z * chunk_size.z)
 		add_child(mi)
 
-	if tasks_remaining == 0:
-		print(pow(render_distance*2+1,2)," Chunks completed in: ",Time.get_ticks_msec()-t_start)
-
 ## Initializes the terrain generation.
 func _ready() -> void:
-	
 	SignalBus.sdf_gen_ended.connect(sdf_gen_ended)
 	SignalBus.meshing_ended.connect(meshing_ended)
-	
-	t_start = Time.get_ticks_msec()
 	
 	for cx in range(-render_distance,render_distance+1):
 		for cz in range(-render_distance,render_distance+1):
 			var chunk_key := Vector3i(cx,0,cz)
 			ThreadPool.add_task(TerrainDataGenerator.generate_sdf.bind(t_noise,chunk_size,chunk_key))
-			tasks_remaining += 1
