58cf9915ffbd3dc227111f7ec3b2f2e908a35c9d
Caspianlake <imranhuseynguliyev@gmail.com>
2026-03-09 16:53:57 +0300

    asdad

--- Scripts/Terrain/terrain.gd
+++ Scripts/Terrain/terrain.gd
@@ -1,24 +1,43 @@
-extends Node3D
+extends Node3D 
 
 @export_category("Terrain")
-@export var render_distance: int = 5 ## In chunks
+@export var render_distance: int = 5 
 @export var chunk_size: Vector3i = Vector3i(16,256,16)
 @export var block_size: int = 1
 
-var chunk_list: Array[Chunk] = []
+var chunk_list: Dictionary[Vector3i, Chunk] = Dictionary()
 
 var player: CharacterBody3D
+var player_last_ck: Vector3i
 
 func _ready() -> void:
 	player = get_parent().find_child("Player")
 
-func _process(delta: float) -> void:
-	if player:
-		terrain_process()
 
-func terrain_process() -> void:
-	var player_cvector = TerrainUtil.get_player_chunk(player.position,chunk_size,block_size)
+func _process(_delta: float) -> void:
+	if player:
+		var player_ck: Vector3i = TerrainUtil.get_player_chunk(player.position,chunk_size,block_size)
+		if player_last_ck != player_ck:
+			terrain_process(player_ck)
+		player_last_ck = player_ck
+
+func terrain_process(player_ck: Vector3i) -> void:
+	
+	deactivate()
+	for cx in range(-render_distance, render_distance + 1):
+		for cz in range (-render_distance, render_distance + 1):
+			activate(Vector3i(player_ck.x + cx,0,player_ck.z + cz))
+
+func deactivate() -> void:
+	for ck in chunk_list:
+		chunk_list[ck].active = false
+
+func activate(chunk_key: Vector3i) -> void:
+	if chunk_list[chunk_key]:
+		chunk_list[chunk_key].active = true
+	else:
+		chunk_list[chunk_key] = Chunk.new()
 
 class Chunk:
-	var vector: Vector3i
 	var active: bool = false
+	var status: String = "incomplete"
--- Scripts/Terrain/terrain_data_generator.gd
+++ Scripts/Terrain/terrain_data_generator.gd
@@ -1,32 +1,6 @@
-## Generates terrain data using Signed Distance Fields (SDF) from noise.
 class_name TerrainDataGenerator
 extends Node
 
-## Generates SDF data for a terrain chunk.
-## noise_map: The noise generator to sample heights.
-## chunk_size: Dimensions of the chunk.
-## chunk_key: Position key of the chunk.
-static func generate_sdf(noise_map: FastNoiseLite, chunk_size: Vector3i, chunk_key: Vector3i, amplitude: float):
-	
-	var t_start = Time.get_ticks_msec()
-	
-	var sample_size:= chunk_size + Vector3i(2,2,2)
-	var sdf_data := PackedFloat32Array()
-	sdf_data.resize(sample_size.x*sample_size.y*sample_size.z)
-			
-	var chunk_offset:= Vector3(chunk_key.x * chunk_size.x, 0, chunk_key.z * chunk_size.z)
-	var idx = 0
-			
-			
-	for x in range(sample_size.x):
-		for z in range(sample_size.z):
-			var column_height:= remap(clampf(noise_map.get_noise_2d(chunk_offset.x+x,chunk_offset.z+z),-1,1),-1,1,0,chunk_size.y*amplitude)
-					
-			for y in range(sample_size.y):
-				sdf_data[idx] = y - column_height
-				idx += 1
-	
-	SignalBus.sdf_gen_ended.emit.call_deferred(sdf_data, chunk_key)
-	
-	print("SDF Generated in: ",Time.get_ticks_msec()-t_start)
-	return
+
+static func generate_chunk(chunk_size: Vector3i, chunk_key: Vector3i) -> void:
+	pass
