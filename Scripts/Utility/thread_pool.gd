a658496692d3313f57240e43b5acfc1015709f18
Caspianlake <imranhuseynguliyev@gmail.com>
2026-03-09 15:42:48 +0300

    aqwa2

--- Scripts/Utility/thread_pool.gd
+++ Scripts/Utility/thread_pool.gd
@@ -1,18 +1,11 @@
-## Manages a pool of threads for executing tasks.
 extends Code
 
-## Queue of tasks to execute.
 var queue: Array[Callable] = []
-## Mutex for thread-safe access to the queue.
 var mutex: Mutex
-## Semaphore to signal available work.
 var semaphore: Semaphore
-## Array of active threads.
 var threads: Array[Thread] = []
-## Flag to signal threads to exit.
 var exit_thread: bool = false
 
-## Initializes the thread pool.
 func _init():
 	mutex = Mutex.new()
 	semaphore = Semaphore.new()
@@ -27,17 +20,17 @@ func add_task(task: Callable):
 	mutex.lock()
 	queue.push_back(task)
 	mutex.unlock()
-	semaphore.post() # Wake up one thread
+	semaphore.post() 
 
 ## Main loop for worker threads to process tasks.
 func _thread_loop():
 	while true:
-		semaphore.wait() # Sleep until work is available
+		semaphore.wait() 
 		
 		mutex.lock()
 		if exit_thread:
 			mutex.unlock()
-			return # Exit the loop and kill the thread
+			return 
 		
 		if queue.is_empty():
 			mutex.unlock()
@@ -46,20 +39,16 @@ func _thread_loop():
 		var task = queue.pop_front()
 		mutex.unlock()
 		
-		# Execute the Callable
 		if task.is_valid():
 			task.call()
 
 func _exit_tree():
-	# Clean shutdown
 	mutex.lock()
 	exit_thread = true
 	mutex.unlock()
 	
-	# Wake up all threads so they can see the 'exit_thread' flag
 	for i in threads.size():
 		semaphore.post()
 		
-	# Wait for threads to finish their current task and close
 	for t in threads:
 		t.wait_to_finish()
