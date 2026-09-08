eed94618429343c7300725cec6c0b8c814a66a45
Caspianlake <imranhuseynguliyev@gmail.com>
2026-09-08 04:03:31 +0300

    balls

--- Scripts/multi.gd
+++ Scripts/multi.gd
@@ -1,26 +1,31 @@
-extends MultiMeshInstance3D
+extends Node3D
 
+@export var world_size: int = 32
+@export var grid_size: int = 32
+@export var new_noise: FastNoiseLite = FastNoiseLite.new()
 
 func _ready():
 	var main_mesh: BoxMesh = BoxMesh.new()
+	main_mesh.size = Vector3(1,512,1)
 	main_mesh.surface_set_material(0,load("res://Resources/UniversalMaterial.tres"))
 	
-	# Create the multimesh.
-	multimesh = MultiMesh.new()
-	# Set the format first.
-	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-	# Set the mesh that will be duplicated.
-	multimesh.mesh = main_mesh
-	
-	multimesh.use_colors = true
-	
-	# Then resize (otherwise, changing the format is not allowed).
-	multimesh.instance_count = 2
-	# Maybe not all of them should be visible at first.
-	multimesh.visible_instance_count = 2
-
-	# Set the transform of the instances.
-	for i in multimesh.visible_instance_count:
-		multimesh.set_instance_transform(i, Transform3D(Basis(), Vector3(i * 1, 0, 0)))
-	multimesh.set_instance_color(0,Color8(1,0,0))
-	multimesh.set_instance_color(1,Color8(2,0,0))
+	for cX in range(0,world_size):
+		for cZ in range(0,world_size):
+			var new_multimesh = MultiMesh.new()
+			new_multimesh.transform_format = MultiMesh.TRANSFORM_3D
+			new_multimesh.mesh = main_mesh
+			new_multimesh.use_colors = true
+			new_multimesh.instance_count = grid_size * grid_size
+			new_multimesh.visible_instance_count = grid_size * grid_size
+			
+			var fX = cX * grid_size
+			var fZ = cZ * grid_size
+			
+			for x in range(0,grid_size):
+				for z in range(0, grid_size):
+					var height = floorf(remap(new_noise.get_noise_2d(fX + x,fZ + z),-1.0,1.0,1.0,512.0))
+					new_multimesh.set_instance_transform(x*grid_size + z, Transform3D(Basis(), Vector3(fX + x, height-700, fZ + z)))
+			
+			var mmi = MultiMeshInstance3D.new()
+			mmi.multimesh = new_multimesh
+			add_child(mmi)
